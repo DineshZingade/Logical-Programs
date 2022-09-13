e345afe3619605f8596865d21bd6951f4da715db
DineshZingade <zingadedinesh1991@gmail.com>
2022-09-14 00:03:11 +0530

    Day6Assignment

---
+++ Bridgelabz/PrimeNumber.java
@@ -0,0 +1,33 @@
+package Com.Bridgelabz;
+
+import java.util.Scanner;
+
+//Prime number means greater than 1 nd divisible by itself or 1..7 is prime
+public class PrimeNumber {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter the Number");
+		int num=sc.nextInt();
+  int m=0,i,flag=0;
+  m = num/2;
+    if(num==0 && num==1) {
+    	System.out.println(num +" Is Not prime Number");
+    }else {
+    	for(i = 2;i<=m;i++) {
+    		if(num%i==0) {
+    			flag = 1;
+    			break;
+    			
+    		}
+    	}
+    	if(flag !=0) {
+    		System.out.println(num +"Is Not prime");
+    	}else
+    	{
+    		System.out.println(num +
+    				" Is Prime number");
+    	}
+    }	
+	}
+}
